359bbdcca0eb3389403c94a69255013eeb874d26
garelickja <138186599+garelickja@users.noreply.github.com>
2024-03-12 14:14:54 -0500

    lost context bug

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const messageConfig = {
-  frontContent: "Happy Birthday, Odin One-Eye!",
-  insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
+  frontContent: 'Happy Birthday, Odin One-Eye!',
+  insideContent:
+    "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
   closing: {
-      "Thor": "Admiration, respect, and love",
-      "Loki": "Your son"
+    Thor: 'Admiration, respect, and love',
+    Loki: 'Your son',
   },
-  signatories: [
-      "Thor",
-      "Loki"
-  ]
+  signatories: ['Thor', 'Lok    i'],
 };
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  this.signatories.forEach(function (signatory) {
+    const message = `${this.closing[signatory]}, ${signatory}`;
+    console.log(message);
+  }, this);
+};
+
+printCard.call(messageConfig);
